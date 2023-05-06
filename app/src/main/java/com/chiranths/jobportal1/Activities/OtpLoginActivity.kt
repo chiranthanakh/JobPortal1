2d1e66b8b3786c8e8573ba42389adcc15b1ddb92
@prinshGlobal <prinsh@global>
2023-05-06 17:46:08 +0530

    06 May UI

--- app/src/main/java/com/chiranths/jobportal1/Activities/OtpLoginActivity.kt
+++ app/src/main/java/com/chiranths/jobportal1/Activities/OtpLoginActivity.kt
@@ -49,6 +49,7 @@ class OtpLoginActivity : AppCompatActivity(), View.OnClickListener {
         edt_otp = findViewById(R.id.edt_otp)
         tvSendOtp?.setOnClickListener(this)
         btn_continue?.setOnClickListener(this)
+
         edtPhone?.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
